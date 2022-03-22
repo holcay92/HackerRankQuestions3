db3e5240bf625fa344ce36f5688f6a6c056e136f
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-03-22 14:27:57 +0300

    hexa funtion added

---
+++ string_formatting.py
@@ -0,0 +1,24 @@
+def print_formatted(number):
+    # your code goes here
+    if not 1 <= number <= 99: exit("constrain")
+    result = list()
+    for i in range(number):
+        result.append(i+1)
+    print(result)
+
+def hexa_format(number):
+    str = ""
+    while number != 0:
+        if number % 2 == 1:
+            str += "1"
+        else: str += "0"
+        number = int(number / 2)
+        print(number)
+    str = str[::-1]
+    return print(str)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
+    hexa_format(n)
